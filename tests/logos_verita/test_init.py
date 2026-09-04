3d94d302cb6f761088d7ca24a83b603af86bf2ee
yn.coder@gmail.com <yn.coder@gmail.com>
2026-09-04 22:00:23 +0300

    #2 test

--- tests/logos_verita/test_init.py
+++ tests/logos_verita/test_init.py
@@ -1,6 +1,8 @@
-# test_abc_stc_script.py
-
 import pytest
+
+from litestar import Litestar
+from litestar.status_codes import HTTP_200_OK
+from litestar.testing import AsyncTestClient
 
 from space_station_stc.orion_manuscript.abc_stc_script import ABC_STC_Script, UnknownCommandError
 
@@ -12,3 +14,19 @@ async def test_init():
 
     lv = Logos_Verita_Plugin()
     assert lv.fuser_title == 'Logos Verita reporting center'
+
+@pytest.fixture
+def app() -> Litestar:
+    return Litestar(
+        route_handlers=[Logos_Verita_Controller],
+    )
+
+
+@pytest.mark.asyncio
+async def test_user_homepage(app: Litestar) -> None:
+    async with AsyncTestClient(app) as client:
+        response = await client.get("/lv/")
+
+    assert response.status_code == HTTP_200_OK
+    assert response.text == "Hello!"
+
